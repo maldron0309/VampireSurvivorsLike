2fdf227f9155ae43b21fcab0f5e3df7d5d73b8ef
maldron0309 <bagjinhyeong640@gmail.com>
2024-03-08 10:20:27 +0900

    Feat: Update movement code and animations
    Updated movement code to input system. Connected animations and implemented animation overrides for other characters.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -6,21 +7,38 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
 
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void OnMove(InputValue value)
     {
-        InputVec.x = Input.GetAxisRaw("Horizontal");
-        InputVec.y = Input.GetAxisRaw("Vertical");
+        InputVec = value.Get<Vector2>();
     }
+
 
     private void FixedUpdate()
     {
-        Vector2 nextVec = InputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = InputVec * speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + nextVec);
+    }
+
+    /// <summary>
+    /// LateUpdate is called every frame, if the Behaviour is enabled.
+    /// It is called after all Update functions have been called.
+    /// </summary>
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", InputVec.magnitude);
+        if (InputVec.x != 0)
+        {
+            spriteRenderer.flipX = InputVec.x < 0;
+        }
     }
 }
